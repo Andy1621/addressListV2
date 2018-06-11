80e22a765c7e97baec5bd4f8088fb99e3f537ff5
Andy1621 <812487273@qq.com>
2018-06-11 17:47:07 +0800

    修改改变、增加用户关系接口

--- server/controllers/changeUUship.js
+++ server/controllers/changeUUship.js
@@ -17,64 +17,39 @@ module.exports = async (ctx, next) => {
   let userB_id = ctx.request.body.userB_id;
   let type = ctx.request.body.type;
 
-  if (type == 'normal') {
-    await dbnnn(config.UUship).insert({ userS_id: userS_id, userB_id: userB_id, type: 'normal' })
+  var temp;
+  await dbnnn(config.UUship).where({ userS_id: userS_id, userB_id: userB_id }).select()
+    .catch(function (e) {
+      console.error(e);
+    })
+    .then(
+    function (data) {
+      console.log(data);
+      temp = data;
+      console.log("获取关系成功")
+    });
+  if (temp.length == 0) {
+    await dbnnn(config.UUship).insert({ userS_id: userS_id, userB_id: userB_id, type: type })
       .catch(function (e) {
         console.error(e);
       })
       .then(
       function (data) {
         console.log(data);
-        console.log("添加到名片夹成功")
+        console.log("增加关系成功")
       });
   }
-  else if (type == 'special') {
-    await dbnnn(config.UUship).where({ userS_id: userS_id, userB_id: userB_id }).update({ type: 'special' })
+  else {
+    await dbnnn(config.UUship).where({ userS_id: userS_id, userB_id: userB_id }).update({ type: type })
       .catch(function (e) {
         console.error(e);
       })
       .then(
       function (data) {
         console.log(data);
-        console.log("添加到特殊关心成功")
+        console.log("修改关系成功")
       });
   }
-  else if (type == 'black') {
-    var temp;
-    await dbnnn(config.UUship).where({ userS_id: userS_id, userB_id: userB_id }).select()
-      .catch(function (e) {
-        console.error(e);
-      })
-      .then(
-      function (data) {
-        console.log(data);
-        temp = data;
-        console.log("获取关系成功")
-      });
-    if (temp.length == 0) {
-      await dbnnn(config.UUship).insert({ userS_id: userS_id, userB_id: userB_id, type: 'black' })
-        .catch(function (e) {
-          console.error(e);
-        })
-        .then(
-        function (data) {
-          console.log(data);
-          console.log("添加到黑名单成功")
-        });
-    }
-    else{
-      await dbnnn(config.UUship).where({ userS_id: userS_id, userB_id: userB_id }).update({ type: 'black' })
-        .catch(function (e) {
-          console.error(e);
-        })
-        .then(
-        function (data) {
-          console.log(data);
-          console.log("添加到黑名单成功")
-        });
-    }
-  }
-
 
   return ctx.response.body = ctx.request.body;
 }
